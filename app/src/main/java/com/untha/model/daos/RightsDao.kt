6611e278f47ff33ecea25813068a8bd40935cf16
Gabriela Guamán <gabriela.guaman@thoughtworks.com>
2019-09-23 13:00:29 -0500

    [#5] @cvalarezo -agregar imagenes y ordenar

--- app/src/main/java/com/untha/model/daos/RightsDao.kt
+++ app/src/main/java/com/untha/model/daos/RightsDao.kt
@@ -8,7 +8,7 @@ import com.untha.model.models.Category
 @Dao
 interface RightsDao : BaseDao<Category> {
 
-    @Query("SELECT * FROM Category WHERE type='derechos' ORDER BY id DESC")
+    @Query("SELECT * FROM Category WHERE type='derechos' ORDER BY id ASC")
     fun getAll(): LiveData<List<Category>>
 
 }
