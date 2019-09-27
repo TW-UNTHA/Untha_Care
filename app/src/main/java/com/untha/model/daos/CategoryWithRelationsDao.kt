847931fc8f3b67c05a4b2e8814f617fcaa85d395
Gabriela Guamán <gabriela.guaman@thoughtworks.com>
2019-09-27 15:25:00 -0500

    [#5] @cvalarezo borrar abstract y transition de CategoryWithRelationsDao

--- app/src/main/java/com/untha/model/daos/CategoryWithRelationsDao.kt
+++ app/src/main/java/com/untha/model/daos/CategoryWithRelationsDao.kt
@@ -4,16 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.untha.model.models.QueryingCategory
-import androidx.room.Transaction
 
 @Dao
-abstract class CategoryWithRelationsDao {
+interface CategoryWithRelationsDao {
 
-    @Transaction
     @Query("SELECT * FROM CategoryModel")
-    abstract fun getAllCategories(): LiveData<List<QueryingCategory>>
+    fun getAllCategories(): LiveData<List<QueryingCategory>>
 
-    @Transaction
+
     @Query("SELECT * FROM CategoryModel WHERE parent_id=2 ORDER BY id ASC")
-    abstract fun getAllRightsCategoriess(): LiveData<List<QueryingCategory>>
+    fun getAllRightsCategoriess(): LiveData<List<QueryingCategory>>
 }
