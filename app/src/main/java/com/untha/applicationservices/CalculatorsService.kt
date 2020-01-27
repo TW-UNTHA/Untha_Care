edc1b8a082c67e7366050e69296862de662024b5
Michelle Peralbo <michelle.peralvo@thoughtworks.com>
2020-01-27 12:48:41 -0500

    @cronopio94 se anade servicio para metodos de calculadoras

---
+++ app/src/main/java/com/untha/applicationservices/CalculatorsService.kt
@@ -0,0 +1,10 @@
+package com.untha.application_services
+
+
+
+class CalculatorsService {
+    fun getVacations(){
+        println("This is a test")
+    }
+
+}
---
+++ app/src/test/java/com/untha/applicationservices/CalculatorsServiceTest.kt
@@ -0,0 +1,16 @@
+package com.untha.application_services
+
+import org.junit.Assert.*
+import org.junit.Ignore
+import org.junit.Test
+
+
+class CalculatorsServiceTest{
+
+    @Ignore
+    fun `should return days`(){
+        val calculator = CalculatorsService()
+        calculator.getVacations()
+    }
+
+}
