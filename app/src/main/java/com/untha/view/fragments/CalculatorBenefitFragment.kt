9c2e4974d07dbcaeeb819bb8f15ec6be0d838a32
PaolaCajilemaTW <tania.c@thoughtworks.com>
2020-02-11 11:47:15 -0500

    [#6] @cronopio94 @PaolaCajilemaTW - preparacion de datos para enviarlos a la pantalla de resultados

--- app/src/main/java/com/untha/view/fragments/CalculatorBenefitFragment.kt
+++ app/src/main/java/com/untha/view/fragments/CalculatorBenefitFragment.kt
@@ -8,6 +8,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.navigation.findNavController
 import com.untha.R
 import com.untha.applicationservices.CalculatorsService
 import com.untha.utils.Constants
@@ -23,6 +24,11 @@ class CalculatorBenefitFragment : BaseFragment() {
         const val RANGE_LAST_DAY_OF_MONTH = 32
         const val LAST_FIVE_YEARS = 5
         const val MONTH_WITH_TWO_DIGITS = 10
+        const val PARTIAL_TIME = 1
+        const val COMPLETE_TIME = 2
+        const val MAX_PARTIAL_HOURS = 36
+        const val SIERRA_ORIENTE = 2
+        const val COSTA_GALAPAGOS = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +58,117 @@ class CalculatorBenefitFragment : BaseFragment() {
         )
         loadAllSpinners()
 
+        changeAppearancePartialHoursThroughtWorkday()
+
+        changeStateSpinnersEndDate()
+
+        btnCalcular.setOnClickListener {
+            val startDate = getStartDateTransformed()
+            val endDate = if (checkBoxEndDate.isChecked) Calendar.getInstance() else getEndDate()
+            val validDates = validationDates(startDate, endDate)
+            val validSalary = validationSalaryInput()
+            val validWorkday = validationSpinnerWorkday()
+            if (validDates && validSalary && validWorkday) {
+                val verifiedStartDate =
+                    buildDate(spinnerStartDateYear, spinnerStartDateMonth, spinnerStartDateDay)
+                val verifiedEndDate = endDateToString(endDate)
+                val area = getArea()
+                val hours = getHours()
+                val workday = getWorkday()
+                val salary = inputSalary.text.toString()
+
+                val bundle = Bundle().apply {
+                    putString("startDate", verifiedStartDate)
+                    putString("endDate", verifiedEndDate)
+                    putString("salary", salary)
+                    putInt("hours", hours)
+                    putInt("idWorkday", workday)
+                    putInt("idArea", area)
+                }
+                view.findNavController().navigate(
+                    0, bundle,
+                    navOptions, null
+                )
+            }
+
+        }
+    }
+
+    private fun getWorkday() = if (spinnerWorkday.selectedItemPosition == PARTIAL_TIME)
+        PARTIAL_TIME else PARTIAL_TIME
+
+    private fun getHours() = if (spinnerWorkday.selectedItemPosition == PARTIAL_TIME)
+        inputPartialHours.text.toString().toInt() else 0
+
+    private fun getArea() = if (spinnerArea.selectedItemPosition == 0)
+        SIERRA_ORIENTE else COSTA_GALAPAGOS
+
+    private fun endDateToString(endDate: Calendar): String {
+        return endDate.get(Calendar.YEAR).toString().plus("-")
+            .plus(transformationMonth(endDate.get(Calendar.MONTH))).plus("-")
+            .plus(addZero(endDate.get(Calendar.DAY_OF_MONTH)))
+    }
+
+    private fun getStartDateTransformed(): Calendar {
+        validationStartDate()
+        val startDate = transformToCalendarDate(
+            spinnerStartDateYear,
+            spinnerStartDateMonth,
+            spinnerStartDateDay
+        )
+        return startDate
+    }
+
+    private fun validationDates(startDate: Calendar, endDate: Calendar): Boolean {
+        if (startDate.after(endDate)) {
+            showToast(R.string.wrong_date_compare_dates)
+            return false
+        }
+        return true
+    }
+
+    private fun validationStartDate(): Boolean {
+        val isValidDate =
+            isValidDate(spinnerStartDateYear, spinnerStartDateMonth, spinnerStartDateDay)
+        if (isValidDate) {
+            showToast(R.string.wrong_date_start)
+            return false
+        }
+        return true
+    }
+
+    private fun validationSalaryInput(): Boolean {
+        if (inputSalary.text.toString().isEmpty()) {
+            showToast(R.string.wrong_salary_empty)
+            return false
+        }
+        val salary = inputSalary.text.toString().toInt()
+        if (salary == 0) {
+            showToast(R.string.wrong_salary_zero)
+            return false
+        }
+        return true
+    }
+
+    private fun validationSpinnerWorkday(): Boolean {
+        if (spinnerWorkday.selectedItemPosition == PARTIAL_TIME) {
+            if (inputPartialHours.text.toString().isEmpty()) {
+                showToast(R.string.wrong_number_of_hours_minimun)
+                return false
+            }
+            if (inputPartialHours.text.toString().toInt() == 0) {
+                showToast(R.string.wrong_number_of_hours_zero)
+                return false
+            }
+            val hours = inputPartialHours.text.toString().toInt()
+            if (hours > MAX_PARTIAL_HOURS) {
+                showToast(R.string.wrong_number_of_hours_max)
+            }
+        }
+        return true
+    }
+
+    private fun changeAppearancePartialHoursThroughtWorkday() {
         spinnerWorkday.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 println("nothing selected")
@@ -63,62 +180,39 @@ class CalculatorBenefitFragment : BaseFragment() {
                 position: Int,
                 id: Long
             ) {
-                if (position != 0) {
-                    inputPartialHours.visibility = View.VISIBLE
-                } else {
-                    inputPartialHours.visibility = View.GONE
-                }
+                inputPartialHours.visibility =
+                    if (position == PARTIAL_TIME) View.VISIBLE else View.GONE
             }
         }
+    }
 
+    private fun changeStateSpinnersEndDate() {
         checkBoxEndDate.setOnClickListener {
             if (checkBoxEndDate.isChecked) {
-                changeStateSpinnersEndDate(spinnerEndDateDay, false)
-                changeStateSpinnersEndDate(spinnerEndDateMonth, false)
-                changeStateSpinnersEndDate(spinnerEndDateYear, false)
+                changeStateSpinners(spinnerEndDateDay, false)
+                changeStateSpinners(spinnerEndDateMonth, false)
+                changeStateSpinners(spinnerEndDateYear, false)
             } else {
-                changeStateSpinnersEndDate(spinnerEndDateDay, true)
-                changeStateSpinnersEndDate(spinnerEndDateMonth, true)
-                changeStateSpinnersEndDate(spinnerEndDateYear, true)
+                changeStateSpinners(spinnerEndDateDay, true)
+                changeStateSpinners(spinnerEndDateMonth, true)
+                changeStateSpinners(spinnerEndDateYear, true)
             }
-        }
-
-        btnCalcular.setOnClickListener {
-            if (isValidDate(spinnerStartDateYear, spinnerStartDateMonth, spinnerStartDateDay)
-            ) {
-                showToast(R.string.wrong_date_start)
-            }
-            lateinit var endDate: Calendar
-            if (checkBoxEndDate.isChecked) {
-                endDate = Calendar.getInstance()
-            } else {
-                endDate = getEndDate()
-            }
-            val startDate = transformToCalendarDate(
-                spinnerStartDateYear,
-                spinnerStartDateMonth,
-                spinnerStartDateDay
-            )
-            if (startDate.compareTo(endDate) > 0) {
-                showToast(R.string.wrong_date_compare_dates)
-            }
-
         }
     }
 
     private fun getEndDate(): Calendar {
-        var endDate1: Calendar = Calendar.getInstance()
+        var date: Calendar = Calendar.getInstance()
         if (isValidDate(spinnerEndDateYear, spinnerEndDateMonth, spinnerEndDateDay)
         ) {
             showToast(R.string.wrong_date_end)
         } else {
-            endDate1 = transformToCalendarDate(
+            date = transformToCalendarDate(
                 spinnerEndDateYear,
                 spinnerEndDateMonth,
                 spinnerEndDateDay
             )
         }
-        return endDate1
+        return date
     }
 
     private fun loadAllSpinners() {
@@ -140,12 +234,22 @@ class CalculatorBenefitFragment : BaseFragment() {
         spinnerMonth: Spinner,
         spinnerDay: Spinner
     ): Calendar {
-        val transformMonth = transformationMonth(spinnerMonth)
-        val stringDate = spinnerYear.selectedItem.toString().plus("-")
+        val buildDate = buildDate(spinnerYear, spinnerMonth, spinnerDay)
+        return calculatorService.stringToCalendar(buildDate)
+    }
+
+    private fun buildDate(
+        spinnerYear: Spinner,
+        spinnerMonth: Spinner,
+        spinnerDay: Spinner
+    ): String {
+        val positionIncremented = spinnerMonth.selectedItemPosition
+
+        val transformMonth = transformationMonth(positionIncremented)
+        val buildDate = spinnerYear.selectedItem.toString().plus("-")
             .plus(transformMonth).plus("-")
-            .plus(spinnerDay.selectedItem.toString())
-        val date = calculatorService.stringToCalendar(stringDate)
-        return date
+            .plus(addZero(spinnerDay.selectedItem.toString().toInt()))
+        return buildDate
     }
 
     private fun showToast(idString: Int) {
@@ -156,19 +260,21 @@ class CalculatorBenefitFragment : BaseFragment() {
         ).show()
     }
 
-    private fun changeStateSpinnersEndDate(spinner: Spinner, state: Boolean) {
+    private fun changeStateSpinners(spinner: Spinner, state: Boolean) {
         spinner.isEnabled = state
         spinner.isClickable = state
     }
 
-    private fun transformationMonth(spinnerMonth: Spinner): String {
-        val month = spinnerMonth.selectedItemPosition + 1
-        val addZeroToMonth = if (month < MONTH_WITH_TWO_DIGITS) {
-            ("0").plus(month)
-        } else {
-            month
-        }
-        return addZeroToMonth.toString()
+    private fun transformationMonth(month: Int): String {
+        val positionIncremented = month + 1
+        val addZeroToMonth = addZero(positionIncremented)
+
+        return addZeroToMonth
+    }
+
+    private fun addZero(number: Int): String {
+        return if (number < MONTH_WITH_TWO_DIGITS) ("0").plus(number)
+        else number.toString()
     }
 
     private fun isValidDate(
@@ -176,22 +282,17 @@ class CalculatorBenefitFragment : BaseFragment() {
         spinnerMonth: Spinner,
         spinnerDay: Spinner
     ): Boolean {
-        val yearMonth = Calendar.getInstance()
-        yearMonth[Calendar.MONTH] = spinnerMonth.selectedItemPosition
-        yearMonth[Calendar.YEAR] =
-            spinnerYear.selectedItem.toString().toInt()
+        val dateSelected = Calendar.getInstance()
+        dateSelected[Calendar.MONTH] = spinnerMonth.selectedItemPosition
+        dateSelected[Calendar.YEAR] = spinnerYear.selectedItem.toString().toInt()
 
         val isMoreThanDayLimit =
-            spinnerDay.selectedItem.toString().toInt() > getLastDayOfMonth(
-                yearMonth
-            )
+            spinnerDay.selectedItem.toString().toInt() > getLastDayOfMonth(dateSelected)
         return isMoreThanDayLimit
     }
 
 
-    private fun getLastDayOfMonth(date: Calendar): Int {
-        return date.getActualMaximum((Calendar.DAY_OF_MONTH))
-    }
+    private fun getLastDayOfMonth(date: Calendar) = date.getActualMaximum((Calendar.DAY_OF_MONTH))
 
 
     private fun loadYearsAdapter(spinner: Spinner) {
@@ -213,8 +314,8 @@ class CalculatorBenefitFragment : BaseFragment() {
 
     private fun loadSpinnerDays(): List<Int> {
         val days = mutableListOf<Int>()
-        for (i in 1 until RANGE_LAST_DAY_OF_MONTH) {
-            days.add(i)
+        for (day in 1 until RANGE_LAST_DAY_OF_MONTH) {
+            days.add(day)
         }
         return days
     }
@@ -222,8 +323,8 @@ class CalculatorBenefitFragment : BaseFragment() {
     private fun loadSpinnerYear(): List<Int> {
         val currentYear = Calendar.getInstance().get(Calendar.YEAR)
         val years = mutableListOf<Int>()
-        for (i in currentYear downTo (currentYear - LAST_FIVE_YEARS)) {
-            years.add(i)
+        for (year in currentYear downTo (currentYear - LAST_FIVE_YEARS)) {
+            years.add(year)
         }
         return years
     }
