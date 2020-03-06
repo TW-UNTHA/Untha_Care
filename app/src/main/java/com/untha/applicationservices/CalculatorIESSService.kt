ad37850d3b4eec540b06ba20437e5e5b209bc69e
PaolaCajilemaTW <tania.c@thoughtworks.com>
2020-03-06 17:21:00 -0500

    [#36] @PaolaCajilemaTW @cronopio94 - Se realizan calculos de IESS y fondos de reserva por dias trabajados y sus pruebas unitarias

--- app/src/main/java/com/untha/applicationservices/CalculatorIESSService.kt
+++ app/src/main/java/com/untha/applicationservices/CalculatorIESSService.kt
@@ -1,9 +1,11 @@
 package com.untha.applicationservices
 
+import com.untha.utils.ConstantsCalculators.DAYS_IN_MONTH
 import com.untha.utils.ConstantsCalculators.DAYS_OF_YEAR
 import com.untha.utils.ConstantsCalculators.PERCENTAJE_APORTE_FONDOS_RESERVA
 import com.untha.utils.ConstantsCalculators.PERCENTAJE_APORTE_IESS_PRIVADO
 import com.untha.utils.calculateNumberOfDayBetween
+import com.untha.utils.numberDaysWorked
 import com.untha.utils.stringToCalendar
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -32,5 +34,36 @@ class CalculatorIESSService {
         } else {
             return 0.00.toBigDecimal().setScale(2, RoundingMode.HALF_UP)
         }
+    }
+
+    fun getFondoReservaMensualizadoFiniquito(
+        startDate: String,
+        endDate: String,
+        salary: BigDecimal
+    ): BigDecimal? {
+        val numberOfDaysBetweenDates =
+            calculateNumberOfDayBetween(stringToCalendar(startDate), stringToCalendar(endDate))
+        if (numberOfDaysBetweenDates > DAYS_OF_YEAR) {
+            val numberDaysWorked = numberDaysWorked(endDate, startDate)
+            val salaryForDay = salary.divide(DAYS_IN_MONTH.toBigDecimal())
+            val result =
+                numberDaysWorked.toBigDecimal() * salaryForDay * PERCENTAJE_APORTE_FONDOS_RESERVA.toBigDecimal()
+            return result.setScale(2, RoundingMode.HALF_UP)
+        }
+        return 0.00.toBigDecimal().setScale(2, RoundingMode.HALF_UP)
+
+    }
+
+    fun getAportacionMensualIESSFiniquito(
+        startDate: String,
+        endDate: String,
+        salary: BigDecimal
+    ): BigDecimal {
+        val numberDaysWorked = numberDaysWorked(endDate, startDate)
+        val salaryForDay = salary.divide(DAYS_IN_MONTH.toBigDecimal())
+
+        val salaryForDayWorked =
+            salaryForDay * numberDaysWorked.toBigDecimal() * PERCENTAJE_APORTE_IESS_PRIVADO.toBigDecimal()
+        return salaryForDayWorked.setScale(2, RoundingMode.HALF_UP)
     }
 }
--- app/src/test/java/com/untha/applicationservices/CalculatorIESSServiceTest.kt
+++ app/src/test/java/com/untha/applicationservices/CalculatorIESSServiceTest.kt
@@ -74,4 +74,92 @@ class CalculatorIESSServiceTest {
 
         assertEquals(expectedValue, result)
     }
+
+    @Test
+    fun `should return contribution IESS finiquito with 2019-03-01 and 2020-03-02`() {
+        val expectedValue = BigDecimal(1.89).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(300.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2019-03-01"
+        val endDate = "2020-03-02"
+
+        val result =
+            calculatorIESSService.getAportacionMensualIESSFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+
+    @Test
+    fun `should return contribution IESS finiquito with 2019-03-01 and 2020-03-25`() {
+        val expectedValue = BigDecimal(23.63).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(300.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2019-03-01"
+        val endDate = "2020-03-25"
+
+        val result =
+            calculatorIESSService.getAportacionMensualIESSFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+
+    @Test
+    fun `should return contribution IESS finiquito with 2019-02-01 and 2020-02-29`() {
+        val expectedValue = BigDecimal(28.35).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(300.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2019-02-01"
+        val endDate = "2020-02-29"
+
+        val result =
+            calculatorIESSService.getAportacionMensualIESSFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+
+
+    @Test
+    fun `should return contribution IESS finiquito with 2020-02-04 and 2020-02-21`() {
+        val expectedValue = BigDecimal(17.01).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(300.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2020-02-04"
+        val endDate = "2020-02-21"
+
+        val result =
+            calculatorIESSService.getAportacionMensualIESSFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+
+    @Test
+    fun `should return 0 of fondo de reserva contribution finiquito when period is less than one year`() {
+        val expectedValue = BigDecimal(0.00).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(450.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2019-01-01"
+        val endDate = "2019-12-30"
+
+        val result = calculatorIESSService.getFondoReservaMensualizadoFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+    @Test
+    fun `should fondo de reserva contribution finiquito when period more than one year start date 2018-11-01 end date 2019-12-30`() {
+        val expectedValue = BigDecimal(37.49).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(450.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2018-11-01"
+        val endDate = "2019-12-30"
+
+        val result = calculatorIESSService.getFondoReservaMensualizadoFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
+
+    @Test
+    fun `should fondo de reserva contribution finiquito when period more than one year start date 2019-05-14 end date 2019-05-28`() {
+        val expectedValue = BigDecimal(0.00).setScale(2, RoundingMode.HALF_UP)
+        val salary = BigDecimal(450.00).setScale(2, RoundingMode.HALF_UP)
+        val startDate = "2019-05-14"
+        val endDate = "2019-05-28"
+
+        val result = calculatorIESSService.getFondoReservaMensualizadoFiniquito(startDate, endDate, salary)
+
+        assertEquals(expectedValue, result)
+    }
 }
